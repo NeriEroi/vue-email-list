15f5a25d891ed6ebff4cfaff133f05c1553e54d9
Michele <micheleierniero@gmail.com>
2021-09-17 09:43:38 +0200

    emails stampate tutte insieme

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ const app = new Vue({
     methods: {
     },
     mounted(){
-        // chiamata giocatore
         for (i = 0; i < 10; i++) {
             axios
             .get("https://flynn.boolean.careers/exercises/api/random/mail")
